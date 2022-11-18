78a998db3a410e4ddbd20b0bdc4380e1c7976d3d
Hania2004 <117639562+Hania2004@users.noreply.github.com>
2022-11-18 12:56:43 +0100

    Update playlists.dart

--- lib/screens/playlists.dart
+++ lib/screens/playlists.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class Playlists extends StatelessWidget {
-  const Playlists({super.key});
+  const Playlists({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Biblioteka'));
+    return MaterialApp(
+      title: 'Playlists',
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: PreferredSize(preferredSize: const Size.fromHeight(65.0),
+         child: AppBar(
+          backgroundColor: Colors.black,
+           leading: IconButton(
+             icon: const Icon(Icons.arrow_back, size: 35),
+            onPressed: () {},
+         ),
+           title: const Text('Playlisty', style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.add, size: 35),
+                onPressed: () {}
+              )
+            ],
+          )
+        )
+          ),
+      );
   }
 }
