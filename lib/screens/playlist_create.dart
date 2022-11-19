f608fbd80a0f23be9976fcaeb83382e8b2b2be02
MichalUlrych <117636015+MichalUlrych@users.noreply.github.com>
2022-11-19 16:55:06 +0100

    Update playlist_create.dart

--- lib/screens/playlist_create.dart
+++ lib/screens/playlist_create.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 
 class PlaylistCreate extends StatelessWidget {
-  const PlaylistCreate({super.key});
+  const PlaylistCreate ({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+
+        home: Scaffold(
+            backgroundColor: Colors.black,
+            appBar: PreferredSize(preferredSize: const Size.fromHeight(65.0),
+                child: AppBar(
+                  backgroundColor: Colors.black,
+                  centerTitle: true,
+                  title: const Text(
+                    'Tworzenie playlisty',
+                  ),
+                  leading: IconButton(
+                    icon: const Icon(Icons.arrow_back, size: 35,),
+                    onPressed: () {},
+                  ),
+                )
+            )
+        )
+    );
   }
 }
