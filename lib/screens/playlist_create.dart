135f2ee84ccc20b50705981982acb14f53f04682
MichalUlrych <117636015+MichalUlrych@users.noreply.github.com>
2022-11-21 23:08:15 +0100

    Update playlist_create.dart

--- lib/screens/playlist_create.dart
+++ lib/screens/playlist_create.dart
@@ -26,3 +26,26 @@ class PlaylistCreate extends StatelessWidget {
     );
   }
 }
+
+final _textController = TextEditingController();
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    body: Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          TextField(
+            controller: _textController,
+            decoration: InputDecoration(
+                hintText: 'Nazwa',
+                border:  OutlineInputBorder(),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
