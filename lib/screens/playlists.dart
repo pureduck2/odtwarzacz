d9d290ce0d9b0b63910e4911e42ee812dbb20afb
Hania2004 <117639562+Hania2004@users.noreply.github.com>
2022-11-21 09:41:12 +0100

    Update playlists.dart

--- lib/screens/playlists.dart
+++ lib/screens/playlists.dart
@@ -9,31 +9,36 @@ class Playlists extends StatelessWidget {
       title: 'Playlists',
       home: Scaffold(
         backgroundColor: Colors.white,
-        appBar: PreferredSize(preferredSize: const Size.fromHeight(65.0),
-         child: AppBar(
-          backgroundColor: Colors.black,
-           leading: IconButton(
-             icon: const Icon(Icons.arrow_back, size: 35),
-            onPressed: () {},
-         ),
-           title: const Text('Playlisty', style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),),
-            centerTitle: true,
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.add, size: 35),
-                onPressed: () {}
-              )
-            ],
-          )
+        appBar: PreferredSize(preferredSize: const Size.fromHeight(60),
+            child: AppBar(
+              backgroundColor: Colors.blueGrey.shade800,
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back, size: 35),
+                onPressed: () {},
+              ),
+              title: const Text('Playlisty', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+              centerTitle: true,
+              actions: [
+                IconButton(
+                    icon: const Icon(Icons.add, size: 35),
+                    onPressed: () {}
+                )
+              ],
+            )
         ),
-          bottomNavigationBar: BottomNavigationBar(
-      items: const [
-             BottomNavigationBarItem(icon: Icon(Icons.home_rounded), label: 'Główna'),
-             BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Biblioteka'),
-             BottomNavigationBarItem(icon: Icon(Icons.account_balance_wallet_rounded), label: 'Katalogi'),
-    ],),
-
+        bottomNavigationBar: SizedBox (height: 70,
+          child: BottomNavigationBar(
+            backgroundColor: Colors.blueGrey.shade800,
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.white,
+            items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home_rounded), label: 'Główna'),
+            BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Biblioteka'),
+            BottomNavigationBarItem(icon: Icon(Icons.account_balance_wallet_rounded), label: 'Katalogi'),
+          ],),
+           ),
+        
           ),
-      );
+         );
   }
 }
