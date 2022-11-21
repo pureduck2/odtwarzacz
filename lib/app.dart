bd49045f7ef0a631f0f86a686b808f132a9fbb0d
pureduck2 <117641064+pureduck2@users.noreply.github.com>
2022-11-21 22:51:04 +0100

    Add necessary widgets to player screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,31 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:odtwarzacz/screens/player.dart';
+
 import 'screens/directories.dart';
 import 'screens/home.dart';
 import 'screens/playlists.dart';
-import 'screens/player.dart';
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  MyAppState createState() => MyAppState();
-}
-
-class MyAppState extends State<MyApp> {
-  int _index = 0;
-
-  Widget _body() {
-    switch (_index) {
-      case 0:
-        return const Player();
-      case 1:
-        return const Playlists();
-      case 2:
-        return const Directories();
-      default:
-        throw UnimplementedError();
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,38 +21,72 @@ class MyAppState extends State<MyApp> {
             headline1: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
             headline2: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             headline3: TextStyle(fontSize: 20),
+            subtitle1: TextStyle(color: Colors.grey),
           ))
           .copyWith(
               bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-                elevation: 0,
+            elevation: 0,
             backgroundColor: Colors.transparent,
             selectedItemColor: Colors.white,
           )),
       themeMode: ThemeMode.dark,
-      home: Scaffold(
-        body: SafeArea(
-          child: _body(),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-            currentIndex: _index,
-            onTap: (index) {
-              setState(() => _index = index);
-            },
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.home_outlined),
-                  activeIcon: Icon(Icons.home),
-                  label: 'Główna'),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.library_music_outlined),
-                  activeIcon: Icon(Icons.library_music),
-                  label: 'Biblioteka'),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.folder_outlined),
-                  activeIcon: Icon(Icons.folder),
-                  label: 'Katalogi')
-            ]),
+      initialRoute: '/player',
+      routes: {
+        '/': (context) => const Main(),
+        '/player': (context) => const Player(),
+      },
+    );
+  }
+}
+
+class Main extends StatefulWidget {
+  const Main({super.key});
+
+  @override
+  MainState createState() => MainState();
+}
+
+class MainState extends State<Main> {
+  int _index = 0;
+
+  Widget _body() {
+    switch (_index) {
+      case 0:
+        return const Home();
+      case 1:
+        return const Playlists();
+      case 2:
+        return const Directories();
+      default:
+        throw UnimplementedError();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: _body(),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _index,
+          onTap: (index) {
+            setState(() => _index = index);
+          },
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined),
+                activeIcon: Icon(Icons.home),
+                label: 'Główna'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.library_music_outlined),
+                activeIcon: Icon(Icons.library_music),
+                label: 'Biblioteka'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.folder_outlined),
+                activeIcon: Icon(Icons.folder),
+                label: 'Katalogi')
+          ]),
     );
   }
 }
--- lib/screens/player.dart
+++ lib/screens/player.dart
@@ -1,10 +1,61 @@
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/cover.dart';
 
 class Player extends StatelessWidget {
   const Player({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("odtwarzacz"));
+    var textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('album'),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.volume_up_sharp),
+          )
+        ],
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Cover(constraints: null),
+          Text('tytuł', style: textTheme.headline1),
+          Text('autor', style: textTheme.subtitle1),
+          const ProgressBar(
+              progress: Duration(milliseconds: 1000),
+              total: Duration(milliseconds: 5000),
+            timeLabelLocation: TimeLabelLocation.sides,
+            progressBarColor: Colors.white,
+            baseBarColor: Colors.grey,
+            thumbColor: Colors.white,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(onPressed: () {}, icon: const Icon(Icons.skip_previous)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.play_circle)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.skip_next)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(onPressed: () {}, icon: const Icon(Icons.shuffle)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.favorite_border)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.repeat))
+            ],
+          )
+        ],
+      )),
+    );
   }
 }
