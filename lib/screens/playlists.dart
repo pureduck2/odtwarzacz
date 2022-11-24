43c98802e179de85843601220b03ec9f4585396f
pureduck2 <117641064+pureduck2@users.noreply.github.com>
2022-11-24 12:40:42 +0100

    Fix compilation errors

--- lib/screens/playlists.dart
+++ lib/screens/playlists.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:odtwarzacz/widgets/row_list.dart';
+import '../controllers/albums.dart';
 import '../widgets/album.dart';
 
 class Playlists extends StatelessWidget {
   const Playlists({super.key, this.onAlbumClick});
 
-  final void Function(BuildContext context, String albumName)? onAlbumClick;
+  final void Function(BuildContext context, AlbumData data)? onAlbumClick;
 
   @override
   Widget build(BuildContext context) {
     var textTheme = Theme.of(context).textTheme;
-    return CustomScrollView
-      (slivers: [
+
+    AlbumsController albumsController = Get.find();
+
+    return CustomScrollView(slivers: [
       SliverAppBar(
         pinned: true,
         title: const Text('Playlisty'),
@@ -20,128 +25,62 @@ class Playlists extends StatelessWidget {
           IconButton(
               icon: const Icon(Icons.add, size: 30),
               splashRadius: 25,
-              onPressed: () {}
-          )],
+              onPressed: () {})
+        ],
       ),
       SliverToBoxAdapter(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    const SizedBox(height: 20,),
-                    Album(
-                      image: Image.asset(
-                          'heart.png'),
-                      name: 'Ulubione',
-                      constraints: const BoxConstraints(maxWidth: 140, maxHeight: 140),
-                      onTap: onAlbumClick,
-                    ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    const SizedBox(height: 20,),
-                    Album(
-                      image: Image.asset(
-                          'assets/recent.png'),
-                      name: 'Ostatnio odtwarzane',
-                      constraints: const BoxConstraints(maxWidth: 140, maxHeight: 140),
-                      onTap: onAlbumClick,
-                    ),
-                    ],
-                )
-              ],
-            ),
-            const SizedBox(height: 60,),
-            Text('Pozostałe playlisty', style: textTheme.headline4),
-
-         Row(
+          child: Column(children: [
+        Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-           Column(
-             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-             children: [
-               const SizedBox(height: 15,),
-             Album(
-               image: Image.network(
-                  'https://upload.wikimedia.org/wikipedia/en/2/23/Romance_%28Official_Album_Cover%29_by_Camila_Cabello.png'),
-              name: 'Playlista 1',
-              constraints: const BoxConstraints(maxWidth: 150, maxHeight: 150),
-              onTap: onAlbumClick,
-            ),
-               const SizedBox(height: 15,),
-             Album(
-               image: Image.network(
-                  'https://upload.wikimedia.org/wikipedia/en/6/6a/Danna_Paola_KO_Cover.jpg'),
-              name: 'Playlista 3',
-              constraints: const BoxConstraints(maxWidth: 150, maxHeight: 150),
-              onTap: onAlbumClick,
-            ),
-               const SizedBox(height: 15,),
-               Album(
-                 image: Image.network(
-                     'https://upload.wikimedia.org/wikipedia/en/6/6a/Danna_Paola_KO_Cover.jpg'),
-                 name: 'Playlista 3',
-                 constraints: const BoxConstraints(maxWidth: 150, maxHeight: 150),
-                 onTap: onAlbumClick,
-               ),
-               const SizedBox(height: 15,),
-               Album(
-                 image: Image.network(
-                     'https://upload.wikimedia.org/wikipedia/en/6/6a/Danna_Paola_KO_Cover.jpg'),
-                 name: 'Playlista 3',
-                 constraints: const BoxConstraints(maxWidth: 150, maxHeight: 150),
-                 onTap: onAlbumClick,
-               ),],
-             ),
-
             Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                const SizedBox(height: 15,),
+                const SizedBox(
+                  height: 20,
+                ),
                 Album(
-                  image: Image.network(
-                      'https://ecsmedia.pl/c/16620154144304975-jpg-gallery.big-iext116924838.jpg'),
-                  name: 'Playlista 2',
-                  constraints: const BoxConstraints(maxWidth: 150, maxHeight: 150),
+                  image: Image.asset('heart.png'),
+                  name: 'Ulubione',
+                  constraints:
+                      const BoxConstraints(maxWidth: 140, maxHeight: 140),
                   onTap: onAlbumClick,
                 ),
-                const SizedBox(height: 15,),
-                Album(
-                  image: Image.network(
-                      'https://linkstorage.linkfire.com/medialinks/images/7262305f-c7b7-433c-b0f7-a8c5af658eab/artwork-440x440.jpg'),
-                  name: 'Playlista 4',
-                  constraints: const BoxConstraints(maxWidth: 150, maxHeight: 150),
-                  onTap: onAlbumClick,
-                ),
-                const SizedBox(height: 15,),
-                Album(
-                  image: Image.network(
-                      'https://upload.wikimedia.org/wikipedia/en/6/6a/Danna_Paola_KO_Cover.jpg'),
-                  name: 'Playlista 3',
-                  constraints: const BoxConstraints(maxWidth: 150, maxHeight: 150),
-                  onTap: onAlbumClick,
-                ),
-                const SizedBox(height: 15,),
-                Album(
-                  image: Image.network(
-                      'https://upload.wikimedia.org/wikipedia/en/6/6a/Danna_Paola_KO_Cover.jpg'),
-                  name: 'Playlista 3',
-                  constraints: const BoxConstraints(maxWidth: 150, maxHeight: 150),
-                  onTap: onAlbumClick,
-                ),],
+              ],
             ),
-    ]
-    )
-    ]
-    )
-    )
-    ]
-    );
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                Album(
+                  image: Image.asset('assets/recent.png'),
+                  name: 'Ostatnio odtwarzane',
+                  constraints:
+                      const BoxConstraints(maxWidth: 140, maxHeight: 140),
+                  onTap: onAlbumClick,
+                ),
+              ],
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 60,
+        ),
+        Text('Pozostałe playlisty', style: textTheme.headline4),
+        const SizedBox(
+          height: 30,
+        ),
+        RowList(
+          rowCount: 2,
+          children: albumsController
+              .getAll()
+              .map((data) => Album(
+                  name: data.name, image: data.image, onTap: onAlbumClick))
+              .toList(),
+        )
+      ]))
+    ]);
   }
 }
---
+++ lib/widgets/row_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:quiver/iterables.dart';
+
+class RowList extends StatelessWidget {
+  const RowList({super.key, required this.children, required this.rowCount, this.spacing = 30});
+
+  final List<Widget> children;
+  final int rowCount;
+  final double spacing;
+
+  @override
+  Widget build(BuildContext context) {
+    var columns = partition(children, rowCount)
+        .map((chunk) =>
+        Wrap(direction: Axis.horizontal, spacing: spacing, children: chunk))
+        .toList();
+
+    return Wrap(
+      direction: Axis.vertical,
+      spacing: spacing,
+      children: columns,
+    );
+  }
+}
